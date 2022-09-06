6a6d0cf150dc297aa9e1f2792cd4ca56f2c205d2
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-09-06 20:51:46 +0300

    Final vite/.env attempt [fix]

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
-import { loadEnv } from 'vite'
 
 const config: UserConfig = {
 	
 	plugins: [sveltekit()],
 	define: {
 		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-		'env("DATABASE_URL")': JSON.stringify(process.env.DATABASE_URL)
+		'import.meta.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL)
 	}
 }
 
