5a30f5f7a2b0c116e2892be282004a886fcf380d
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-09-06 23:53:36 +0300

    Another .env attempt [fix]

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ const config: UserConfig = {
 	plugins: [sveltekit()],
 	define: {
 		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-		'import.meta.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL)
+		'import.meta.env.DATABASE_URL': process.env.DATABASE_URL
 	}
 }
 
