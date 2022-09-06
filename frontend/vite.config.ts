6d6d7a450ad6a7dafd62a0a82ad142d3db8e4514
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-09-06 23:52:05 +0300

    Another .env attempt [fix]

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
 
-const config = (): UserConfig => {
-	console.log(process.env)
-	console.log(process.env.DATABASE_URL)
-	return {
-		plugins: [sveltekit()],
-		define: {
-			'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-			'import.meta.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL)
-		}
+// import { loadEnv } from 'vite'
+
+// Load env file based on `mode` in the current working directory.
+// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+// const env = loadEnv(mode, process.cwd(), '')
+
+const config: UserConfig = {
+	plugins: [sveltekit()],
+	define: {
+		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
+		'import.meta.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL)
 	}
 }
 
