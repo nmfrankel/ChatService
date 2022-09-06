23914634e86db79791b656ad89e573ee099ad99b
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-09-07 00:00:38 +0300

    Another logging test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
 
-console.log(JSON.stringify(process.env.DATABASE_URL))
+// console.log(JSON.stringify(process.env.DATABASE_URL))
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
