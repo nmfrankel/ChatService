4022dcef5b1b03ea08b6a76f71264db8348c6d7c
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-09-06 19:32:29 +0300

    Another test using vite import.meta.env[key] [fix]

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ const config: UserConfig = {
 	plugins: [sveltekit()],
 	define: {
 		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-		'env("DATABASE_URL")': JSON.stringify(process.env.DATABASE_URL)
+		'import.meta.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL)
 	}
 }
 
