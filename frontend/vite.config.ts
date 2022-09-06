65ebdadd37c7b5ac48a01a065b374098382fa22d
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-09-06 23:58:21 +0300

    Test logging from schema.prisma

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
 
-// import { loadEnv } from 'vite'
-
-// Load env file based on `mode` in the current working directory.
-// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-// const env = loadEnv(mode, process.cwd(), '')
+console.log(JSON.stringify(process.env.DATABASE_URL))
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
 	define: {
 		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-		'import.meta.env.DATABASE_URL': process.env.DATABASE_URL
+		'import.meta.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL)
 	}
 }
 
