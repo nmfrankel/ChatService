5544087adff76f29b486a296e1c454a1cbf0969e
Nosson M Frankel <nossonmfrankel@gmail.com>
2022-09-06 19:44:53 +0300

    Another prisma DB URL attempt [fix]

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
+import { loadEnv } from 'vite'
 
 const config: UserConfig = {
+	
 	plugins: [sveltekit()],
 	define: {
 		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-		'import.meta.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL)
+		'process.env': {...process.env, ...loadEnv('', process.cwd())}
 	}
 }
 
