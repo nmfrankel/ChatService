d6bc4b29ec55fc980daa68dc392c7aa07b2de70a
Nosson M Frankel <nossonmfrankel@gmail.com>
2023-07-09 20:56:00 +0300

    Billy goat for /YehudaReich redirect

---
+++ src/routes/billy/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<meta http-equiv="refresh" content="10; url=https://romemus.org/yehudareich">
+</svelte:head>
+
+<div />
+
+<style>
+	div {
+		position: relative;
+		top: -66px;
+		height: 100vh;
+		width: 100vw;
+		content: '';
+		background: url('/billy.jpg') no-repeat center center;
+		background-size: cover;
+		z-index: 10;
+	}
+</style>
